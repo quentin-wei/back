9324fd38c0d72dec92bbef95a8b96d5ab1663703
weiguitie@163.com <weiguitie@163.com>
2019-12-03 16:31:13 +0800

    cg

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 let http = require('http')
 let users = [
-    { id: 222, name: 'aaaa' },
-    { id:322, name: 'c+aa' },
-	{ id:323, name: 'c+ab' }
+    { id:222, name: '黑哥' },
+    { id:322, name: '卡子' },
+	{ id:323, name: '杰哥' }
 ]
 let server = http.createServer(function (req, res) {
     res.setHeader('Access-Control-Allow-Origin','*')
